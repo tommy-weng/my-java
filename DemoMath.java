1d5c0ce4f1c62ea3e0c1b0ac641f4b82dcb8bfc6
sweng <shiqiang-tommy.weng@nokia-sbell.com>
2019-02-19 20:52:21 +0800

    Submit some JAVA files

---
+++ DemoMath.java
@@ -0,0 +1,9 @@
+public class DemoMath {
+    public static void main (String [] args)
+    {
+        System.out.println("PI = " + Math.PI);
+        System.out.println("sin(PI/2) = " + Math.sin(Math.PI/2));
+        System.out.println("sin(PI/6) = " + Math.sin(Math.PI/6));
+        System.out.println("PI/2 toDegrees= " + Math.toDegrees(Math.PI/2));
+    }
+}
